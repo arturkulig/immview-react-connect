c97938e2b48d0f87b36f5eadf364c7f250ab20a6
Artur Kulig <artur.kulig@swmansion.com>
2016-04-24 10:33:32 +0200

    build

--- dist/immview-react-connect.js
+++ dist/immview-react-connect.js
@@ -54,75 +54,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-	
-	var _react = __webpack_require__(1);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _immview = __webpack_require__(2);
-	
-	var _immutable = __webpack_require__(3);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	function connect(component, sources, processor) {
-	
-	    var ImmviewConnector = _react2.default.createClass({
-	        displayName: 'ImmviewConnector',
-	        componentWillMount: function componentWillMount() {
-	            var _this = this;
-	
-	            this.view = new _immview.View(sources);
-	            this.updateChildProps = function (sourceData, props) {
-	                var childProps = _extends({}, props, deImmubtablize(processor ? processor(sourceData, props) : sourceData));
-	                if (childProps) {
-	                    _this.setState({ childProps: childProps });
-	                }
-	            };
-	
-	            var cancelViewReaction = this.view.subscribe(function (data) {
-	                _this.updateChildProps(data, _this.props);
-	            });
-	
-	            this.destroyConnection = function () {
-	                if (_this.view) {
-	                    cancelViewReaction();
-	                    _this.view.destroy();
-	                    _this.view = null;
-	                    _this.destroyConnection = null;
-	                    _this.updateChildProps = null;
-	                }
-	            };
-	        },
-	        componentWillUnmount: function componentWillUnmount() {
-	            this.destroyConnection && this.destroyConnection();
-	        },
-	        componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-	            this.updateChildProps(this.view.read(), nextProps);
-	        },
-	        render: function render() {
-	            return _react2.default.createElement(component, this.state.childProps, this.state.childProps.children);
-	        }
-	    });
-	
-	    return ImmviewConnector;
-	}
-	
-	function deImmubtablize(d) {
-	    return _immutable.Iterable.isIterable(d) ? d.toObject() : d;
-	}
-	
-	// for commonjs import
-	module.exports = connect;
-	
-	// for es6 import
-	module.exports.connect = connect;
-	
-	// for es6 default import
-	module.exports.default = connect;
+	"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{"default":e}}function connect(e,t,n){var i=_react2["default"].createClass({displayName:"ImmviewConnector",componentWillMount:function(){var e=this;this.view=new _immview.View(t),this.cancelViewReaction=this.view.subscribe(function(t){e.updateChildProps(t,e.props)})},componentWillUnmount:function(){this.destroyConnection()},componentWillReceiveProps:function(e){this.updateChildProps(this.view.read(),e)},shouldComponentUpdate:function(e,t){return this.state!==t},updateChildProps:function(e,t){var i=_extends({},t,deImmubtablize(n?n(e,t):e));i&&this.setState({processorProps:i})},destroyConnection:function(){this.view&&(this.cancelViewReaction&&this.cancelViewReaction(),this.cancelViewReaction=null,this.view.destroy(),this.view=null)},render:function(){return _react2["default"].createElement(e,this.state.processorProps)}});return i}function deImmubtablize(e){return _immutable.Iterable.isIterable(e)?e.toObject():e}var _extends=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var i in n)Object.prototype.hasOwnProperty.call(n,i)&&(e[i]=n[i])}return e},_react=__webpack_require__(1),_react2=_interopRequireDefault(_react),_immview=__webpack_require__(2),_immutable=__webpack_require__(3);module.exports=connect,module.exports.connect=connect,module.exports["default"]=connect;
 
 /***/ },
 /* 1 */
