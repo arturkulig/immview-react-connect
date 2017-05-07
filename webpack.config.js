8c8821be7004ef80f4bd4eb12bac008ebd134e74
Artur Kulig <arturkulig@gmail.com>
2017-05-07 17:25:35 +0200

    build with webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,50 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 const config = {
     entry: path.resolve(__dirname, 'src/index.ts'),
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'immview-react-connect.js',
         library: 'immview-react-connect',
         libraryTarget: 'umd',
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js', '']
+        extensions: ['.tsx', '.ts', '.js']
     },
     externals: {
         react: 'react',
         immview: 'immview',
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader'
+                use: {
+                    loader: 'awesome-typescript-loader'
+                }
             },
         ],
     },
     devtool: 'source-map',
-    plugins: []
-};
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        })
+    ]
+}
 
 if (process.env.NODE_ENV === 'production') {
-    const webpack = require('webpack')
-
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compressor: {
-            pure_getters: true,
-            unsafe: true,
-            unsafe_comps: true,
-            screw_ie8: true,
-            warnings: false
-        }
+    config.plugins.push(new UglifyJSPlugin({
+        mangle: false,
+        comments: false,
+        sourceMap: true,
+        compress: true
     }))
 }
+
 
 module.exports = config
